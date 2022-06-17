b983beae023bf5468ad0b50f11360a2a37de8cad
Dinesh Mutyala <dinesh@trala.com>
2022-06-17 10:22:13 -0500

    Adding in Base Application and Launcher Activity

---
+++ app/src/main/java/com/dinesh/demoforinvoy/DemoForInvoyApplication.kt
@@ -0,0 +1,5 @@
+package com.dinesh.demoforinvoy
+
+import android.app.Application
+
+class DemoForInvoyApplication: Application()
---
+++ app/src/main/java/com/dinesh/demoforinvoy/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dinesh.demoforinvoy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dinesh.demoforinvoy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
